4d837b9ef44a70956c5c10ec1371b938a5e43a04
aproxacs <aproxacs@gmail.com>
2010-04-09 18:20:02 +0900

    added excutable.

--- lib/shift_subtitle.rb
+++ lib/shift_subtitle.rb
@@ -1,3 +1,24 @@
 require "shift_subtitle/options"
 require "shift_subtitle/sub_rip"
 require "shift_subtitle/sub_time"
+
+module ShiftSubtitle
+  def self.run!
+    opt = Options.parse(ARGV)
+    puts "operation:     #{opt.operation}"
+    puts "time:   #{opt.time}"
+    puts "input:      #{opt.input}"
+    puts "output:      #{opt.output}"
+
+    File.open(opt.input) do |is|
+      File.open(opt.output, "w") do |os|
+        while true
+          rip = SubRip.read(is)
+          rip.send("#{opt.operation}_time", opt.time)
+          rip.write(os)
+          break if rip.eof?
+        end
+      end
+    end
+  end
+end
